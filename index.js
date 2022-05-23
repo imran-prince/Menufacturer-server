84f8675f929603a45582d26f77612349478f9ed2
imran-hossain <csebubt35@gmail.com>
2022-05-23 23:34:14 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const app = express()
+const cors=require('cors')
+app.use(cors())
+app.use(express.json())
+require('dotenv').config()
 const port = process.env.PORT || 5000
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3s80f.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ 
+
+
+async function run() {
+    try {
+      await client.connect();
+      const productCollection = client.db("bike-parts").collection('products');
+      
+    } finally {
+ 
+    }
+  }
+  run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Princes parts limited')
